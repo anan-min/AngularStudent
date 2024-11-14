5347a0dee23d8e8ab2ed22f79e6a9a711afbd982
nut <nut999anan@hotmail.com>
2024-11-14 10:30:50 +0700

    setup

---
+++ src/app/components/student-insert/student-insert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-insert',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-insert.component.html',
+  styleUrl: './student-insert.component.css'
+})
+export class StudentInsertComponent {
+
+}
